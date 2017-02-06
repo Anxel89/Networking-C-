c94689a03d3dfb21ceff72c1119d9f6492d26129
Mattew Robison <anxel9302@gmail.com>
2017-02-06 17:03:09 -0500

    Add files via upload

---
+++ client/client/Client.Test/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Client;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Client.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Connect()
+        {
+            var client = new clnt();
+            TcpClient c = clnt.ConnectToServer("192.168.0.13", 8080);
+            Assert.IsTrue(c.Connected);
+            
+        }
+
+        [TestMethod]
+        public void SendData()
+        {
+            var client = new clnt();
+            TcpClient c = clnt.ConnectToServer("192.168.0.13", 8080);
+            Pair data = new Pair();
+            string send = "hello";
+            // used to test primitive version of method before readline was introduced.
+            //data = clnt.SendData(c, send);         
+            Assert.IsTrue(data.St.ToString() ==  "The string was recieved by the server.");
+        }
+
+        [TestMethod]
+        public void GetData()
+        {
+            var client = new clnt();
+            TcpClient c = clnt.ConnectToServer("192.168.0.13", 8080);
+            Pair data = new Pair();
+            string send = "hello";
+            // used to test primitive version of method before readline was introduced.
+            //data = clnt.SendData(c, send);
+            //clnt.ReceiveData(data.St);
+            Assert.IsTrue(c.Connected);    
+        }
+
+        [TestMethod]
+        public void Close()
+        {
+            var client = new clnt();
+            TcpClient c = clnt.ConnectToServer("192.168.0.13", 8080);
+            Pair data = new Pair();
+            string send = "hello";
+            // used to test primitive version of method before readline was introduced.
+            //data = clnt.SendData(c, send);
+            //clnt.ReceiveData(data.St);
+            //clnt.CloseConnection(c);
+            Assert.IsTrue(!c.Connected);
+        }
+    }
+
+
+}
---
+++ client/client/client/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Sockets;
+
+public class Pair
+{
+    Stream st;
+    bool flag = false;
+
+    public Stream St
+    {
+        get { return st; }
+        set { st = value; }
+    }
+
+    public bool Flag
+    {
+        get
+        {
+            return flag;
+        }
+        set
+        {
+            flag = value;
+        }
+    }
+    public Pair()
+    {
+
+    }
+    public Pair(bool flag, Stream st)
+    {
+        this.flag = flag;
+        this.st = st;
+    }
+}
+public class clnt
+{
+    public static void Main()
+    {
+
+        try
+        {
+            TcpClient client = ConnectToServer("192.168.0.13", 8080);            
+            bool close_flag = false;
+            while (!close_flag)
+            {
+                Pair data = new Pair();
+                data  = SendData(client);
+                close_flag = data.Flag;
+                Stream st = data.St;
+                ReceiveData(st);
+            }
+            CloseConnection(client);            
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }
+    }
+
+    public static TcpClient ConnectToServer( string address, int port)
+    {
+        TcpClient client = new TcpClient();
+        Console.WriteLine("Connecting.....");
+        client.Connect(address,port); 
+        Console.WriteLine("Connected");
+        return client;
+    }
+
+    public static Pair SendData(TcpClient client)
+    {
+        Console.Write("Enter the string to be transmitted : ");
+        String str = Console.ReadLine();
+        Stream stm = client.GetStream();
+        ASCIIEncoding asen = new ASCIIEncoding();
+        byte[] data_to_send = asen.GetBytes(str);
+        Console.WriteLine("Transmitting.....");
+        stm.Write(data_to_send, 0, data_to_send.Length);
+        str.ToLower();
+        if(str == "close")
+        {
+            bool flag = true;
+            Pair result = new Pair(flag, stm);
+            return result;
+            
+        }
+        else
+        {
+            bool flag = false;
+            Pair result = new Pair(flag, stm);
+            return result;
+        }
+       
+    }
+
+    public static void ReceiveData(Stream st)
+    {
+        byte[] data_stream = new byte[100];
+        int k = st.Read(data_stream, 0, 100);
+        for (int i = 0; i < k; i++)
+            Console.Write(Convert.ToChar(data_stream[i]));
+    }
+
+    public static void CloseConnection(TcpClient client)
+    {
+        client.Close();
+    }
+}
